ccb720aad5400de491c6a71a2d263dc5734e998f
beepscore <support@beepscore.com>
2009-11-21 10:39:34 -0800

    Search for a different document.

--- Parser2.rb
+++ Parser2.rb
@@ -1,8 +1,10 @@
+require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
 
 # Perform a google search
-doc = Nokogiri::HTML(open('http://google.com/search?q=tenderlove'))
+#doc = Nokogiri::HTML(open('http://google.com/search?q=tenderlove'))
+doc = Nokogiri::HTML(open('http://google.com/search?q=edaya'))
 
 # Print out each link using a CSS selector
 doc.css('h3.r > a.l').each do |link|
