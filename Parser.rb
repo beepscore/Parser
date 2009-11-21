60d5d28cedb27ffa1e30fbb217dd01997194fa2d
beepscore <support@beepscore.com>
2009-11-20 20:42:37 -0800

    Initial commit.  Ref nokogiri.org

---
+++ Parser.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'nokogiri'
+# Ref http://nokogiri.org/
+# http://wiki.github.com/tenderlove/nokogiri
+
+
+doc = Nokogiri::HTML.parse(<<-eohtml)
+<html>
+  <head>
+    <title>Hello World</title>
+  </head>
+  <body>
+    <h1>This is an awesome document</h1>
+    <p>
+      I am a paragraph
+        <a href="http://google.ca">I am a link</a>
+    </p>
+  </body>
+</html>
+eohtml
+
+####
+# Search for nodes by css
+doc.css('p > a').each do |a_tag|
+  puts a_tag.content
+end
+
+####
+# Search for nodes by xpath
+doc.xpath('//p/a').each do |a_tag|
+  puts a_tag.content
+end
+
+####
+# Or mix and match.
+doc.search('//p/a', 'p > a').each do |a_tag|
+  puts a_tag.content
+end
+
+###
+# Find attributes and their values
+doc.search('a').first['href']
