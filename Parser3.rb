333e63f2192ad55bfd5002aa1d120e7f76ce7b3d
beepscore <support@beepscore.com>
2009-11-21 11:27:33 -0800

    Change from HTML to XML.

--- Parser3.rb
+++ Parser3.rb
@@ -5,13 +5,9 @@ require 'nokogiri'
 # Perform a google search
 #doc = Nokogiri::HTML(open('http://google.com/search?q=tenderlove'))
 #doc = Nokogiri::HTML(open('http://google.com/search?q=edaya'))
-doc = Nokogiri::HTML(open('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=kbfi'))
+doc = Nokogiri::XML(open('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=kbfi'))
 puts doc
-# Print out each link using a CSS selector
-# doc.css('h3.r > a.l').each do |link|
-#   puts link.content
-# end
-
+# Print out each link using an xml selector
 doc.xml('node > a.l').each do |link|
   puts link.content
 end
