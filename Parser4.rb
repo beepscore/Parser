635bbf9f55dd7bc3495c5e309384d87eecf20ba0
beepscore <support@beepscore.com>
2009-11-21 14:14:53 -0800

    add parser4

--- Parser4.rb
+++ Parser4.rb
@@ -5,9 +5,26 @@ require 'nokogiri'
 # Perform a google search
 #doc = Nokogiri::HTML(open('http://google.com/search?q=tenderlove'))
 #doc = Nokogiri::HTML(open('http://google.com/search?q=edaya'))
-doc = Nokogiri::XML(open('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=kbfi'))
-puts doc
+doc = Nokogiri::XML(open('iTunes Music Librarytest.xml'))
+#puts doc
 # Print out each link using an xml selector
-puts doc.child.child.node_name()
+# puts doc.child.node_name()
+# puts doc.children
+
+# Search for nodes by xpath
+# doc.children().each do |a_tag|
+#   puts a_tag.content
+# end
+
+# note=doc.search("key") # find all tags with the node_name "key"
+# puts note
+
+# doc.xpath('/').each do |a_tag|
+#   puts a_tag.content
+# end
+
+# Search for nodes by xpath
+puts doc.xpath('/*/*/*/*/key[. = "Artist"]')
+
 
 
